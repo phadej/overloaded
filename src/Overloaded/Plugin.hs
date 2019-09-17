c00aff4b8786e6e492bf14d450c57ad7bb09c335
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-09-17 19:55:59 +0300

    Type-checker plugin to solve HasField

--- src/Overloaded/Plugin.hs
+++ src/Overloaded/Plugin.hs
@@ -1,24 +1,34 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 -- | Overloaded plugin, which makes magic possible.
 module Overloaded.Plugin (plugin) where
 
 import Control.Applicative    ((<|>))
-import Control.Monad          (foldM, when)
+import Control.Monad          (foldM, forM, guard, unless, when)
 import Control.Monad.IO.Class (MonadIO (..))
-import Data.List              (foldl', intercalate)
+import Data.List              (elemIndex, foldl', intercalate)
 import Data.List.Split        (splitOn)
+import Data.Maybe             (mapMaybe)
 import Data.Maybe             (catMaybes)
 
 import qualified Data.Generics as SYB
 
 -- GHC stuff
+import qualified Class
 import qualified ErrUtils   as Err
+import qualified FamInst
+import qualified FamInstEnv
 import qualified Finder
 import qualified GhcPlugins as GHC
-import           HsSyn as GHC
+import           HsSyn      as GHC
 import qualified IfaceEnv
+import qualified RdrName
 import           SrcLoc
+import qualified TcEnv
+import qualified TcEvidence as Tc
+import qualified TcMType
+import qualified TcPluginM  as TC
 import qualified TcRnMonad  as TcM
 import qualified TcRnTypes
 
@@ -66,11 +76,62 @@ import qualified TcRnTypes
 --
 -- * Doesn't desugar inside patterns
 --
+-- == RecordFields
+--
+-- __WARNING__ the type-checker plugin is experimental, it's adviced to use
+--
+-- @
+-- {-\# OPTIONS_GHC -ddump-simpl #-}
+-- @
+--
+-- to avoid surprising segfaults.
+--
+-- === Usage
+--
+-- {-\# OPTIONS -fplugin=Overloaded -fplugin-opt=Overloaded:RecordFields #-}
+--
+-- === Implementation bits
+--
+-- See Note [HasField instances] in "ClsInst", the behavior of this plugin is similar.
+--
+-- The 'GHC.Records.Compat.HasField' class is defined in "GHC.Records.Compat" module of @record-hasfield@ package:
+--
+-- @
+-- class 'GHC.Records.Compat.HasField' {k} x r a | x r -> a where
+--     'GHC.Records.Compat.hasField' :: r -> (a -> r, a)
+-- @
+-- Suppose we have
+--
+-- @
+-- data R y = MkR { foo :: [y] }
+-- @
+--
+-- and @foo@ in scope. We will solve constraints like
+--
+-- @
+-- HasField "foo" (R Int) a
+-- @
+--
+-- by emitting a new wanted constraint
+--
+-- @
+-- [Int] ~# a
+-- @
+--
+-- and building a @HasField@ dictionary out of selector @foo@ appropriately cast.
+--
 plugin :: GHC.Plugin
 plugin = GHC.defaultPlugin
     { GHC.renamedResultAction = pluginImpl
+    , GHC.tcPlugin            = enabled tcPlugin
     , GHC.pluginRecompile     = GHC.purePlugin
     }
+  where
+    enabled p args'
+        | "RecordFields" `elem` args = Just p
+        | otherwise                  = Nothing
+      where
+        args = concatMap (splitOn ":") args'
 
 pluginImpl
     :: [GHC.CommandLineOption]
@@ -176,7 +237,7 @@ pluginImpl args' env gr = do
 -- Args parsing
 -------------------------------------------------------------------------------
 
-parseArgs :: GHC.DynFlags -> [String] -> TcRnTypes.TcM Options
+parseArgs :: forall m. MonadIO m => GHC.DynFlags -> [String] -> m Options
 parseArgs dflags = foldM go0 defaultOptions where
     go0 opts arg = do
         (arg', vns) <- elaborateArg arg
@@ -236,6 +297,8 @@ parseArgs dflags = foldM go0 defaultOptions where
     go opts "TypeSymbols" vns = do
         mvn <- oneName "TypeSymbols" vns
         return $ opts { optTypeSymbols = On mvn }
+    go opts "RecordFields" _ =
+        return $ opts { optRecordFields = True }
 
     go opts s _ = do
         warn dflags noSrcSpan $ GHC.text $ "Unknown Overloaded option " ++  show s
@@ -258,41 +321,43 @@ parseArgs dflags = foldM go0 defaultOptions where
             warn dflags noSrcSpan $ GHC.text $ "Over two names specified for " ++ arg
             return (Just (V2 x y))
 
-    elaborateArg :: String -> TcRnTypes.TcM (String, [VarName])
+    elaborateArg :: String -> m (String, [VarName])
     elaborateArg s = case splitOn "=" s of
         []       -> return ("", [])
         (pfx:xs) -> do
             vns <- traverse parseVarName xs
             return (pfx, catMaybes vns)
 
-    parseVarName :: String -> TcRnTypes.TcM (Maybe VarName)
+    parseVarName :: String -> m (Maybe VarName)
     parseVarName "" = return Nothing
     parseVarName xs = do
         let ps = splitOn "." xs
         return (Just (VN (intercalate "." $ init ps) (last ps)))
 
 data Options = Options
-    { optStrings     :: StrSym
-    , optNumerals    :: NumNat
-    , optChars       :: OnOff VarName
-    , optLists       :: OnOff (V2 VarName)
-    , optIf          :: OnOff VarName
-    , optLabels      :: OnOff VarName
-    , optTypeNats    :: OnOff VarName
-    , optTypeSymbols :: OnOff VarName
+    { optStrings      :: StrSym
+    , optNumerals     :: NumNat
+    , optChars        :: OnOff VarName
+    , optLists        :: OnOff (V2 VarName)
+    , optIf           :: OnOff VarName
+    , optLabels       :: OnOff VarName
+    , optTypeNats     :: OnOff VarName
+    , optTypeSymbols  :: OnOff VarName
+    , optRecordFields :: Bool
     }
   deriving (Eq, Show)
 
 defaultOptions :: Options
 defaultOptions = Options
-    { optStrings     = NoStr
-    , optNumerals    = NoNum
-    , optChars       = Off
-    , optLists       = Off
-    , optIf          = Off
-    , optLabels      = Off
-    , optTypeNats    = Off
-    , optTypeSymbols = Off
+    { optStrings      = NoStr
+    , optNumerals     = NoNum
+    , optChars        = Off
+    , optLists        = Off
+    , optIf           = Off
+    , optLabels       = Off
+    , optTypeNats     = Off
+    , optTypeSymbols  = Off
+    , optRecordFields = False
     }
 
 data StrSym
@@ -526,6 +591,9 @@ overloadedTypeNatsMN =  GHC.mkModuleName "Overloaded.TypeNats"
 overloadedTypeSymbolsMN :: GHC.ModuleName
 overloadedTypeSymbolsMN =  GHC.mkModuleName "Overloaded.TypeSymbols"
 
+overloadedRecordFieldsMN :: GHC.ModuleName
+overloadedRecordFieldsMN =  GHC.mkModuleName "GHC.Records.Compat"
+
 -------------------------------------------------------------------------------
 -- Names
 -------------------------------------------------------------------------------
@@ -607,8 +675,211 @@ debug :: MonadIO m => String -> m ()
 debug _ = pure ()
 
 -------------------------------------------------------------------------------
--- V2
+-- V2 and V4
 -------------------------------------------------------------------------------
 
 data V2 a = V2 a a
   deriving (Eq, Show)
+
+data V4 a = V4 a a a a
+  deriving (Eq, Show)
+
+-------------------------------------------------------------------------------
+-- Type-checker plugin
+-------------------------------------------------------------------------------
+
+newtype PluginCtx = PluginCtx
+    { hasPolyFieldCls :: Class.Class
+    }
+
+tcPlugin :: TcM.TcPlugin
+tcPlugin = TcM.TcPlugin
+    { TcM.tcPluginInit  = tcPluginInit
+    , TcM.tcPluginSolve = tcPluginSolve
+    , TcM.tcPluginStop  = const (return ())
+    }
+
+tcPluginInit :: TC.TcPluginM PluginCtx
+tcPluginInit = do
+    -- TODO: don't fail
+    res <- TC.findImportedModule overloadedRecordFieldsMN Nothing
+    cls <- case res of
+        GHC.Found _ md -> TC.tcLookupClass =<< TC.lookupOrig md (GHC.mkTcOcc "HasField")
+        _              -> do
+            dflags <- TC.unsafeTcPluginTcM GHC.getDynFlags
+            TC.tcPluginIO $ GHC.putLogMsg dflags GHC.NoReason Err.SevError noSrcSpan (GHC.defaultErrStyle dflags) $
+                GHC.text "Cannot find module" GHC.<+> GHC.ppr overloadedRecordFieldsMN
+            fail "panic!"
+
+    return PluginCtx
+        { hasPolyFieldCls = cls
+        }
+
+-- HasPolyField "petName" Pet Pet [Char] [Char]
+tcPluginSolve :: PluginCtx -> TcRnTypes.TcPluginSolver
+tcPluginSolve PluginCtx {..} _ _ wanteds = do
+    -- acquire context
+    dflags      <- TC.unsafeTcPluginTcM GHC.getDynFlags
+    famInstEnvs <- TC.getFamInstEnvs
+    rdrEnv      <- TC.unsafeTcPluginTcM TcM.getGlobalRdrEnv
+
+    solved <- forM wantedsHasPolyField $ \(ct, tys@(V4 _k _name _s a)) -> do
+        -- TC.tcPluginIO $ warn dflags noSrcSpan $
+        --     GHC.text "wanted" GHC.<+> GHC.ppr ct
+
+        m <- TC.unsafeTcPluginTcM $ matchHasField dflags famInstEnvs rdrEnv tys
+        fmap (\evTerm -> (evTerm, ct)) $ forM m $ \(tc, dc, args, fl, _sel_id) -> do
+            -- get location
+            let ctloc = TcM.ctLoc ct
+            -- let l = GHC.RealSrcSpan $ TcM.ctLocSpan ctloc
+
+            -- debug print
+            -- TC.tcPluginIO $ warn dflags l $ GHC.text "DEBUG" GHC.$$ GHC.ppr dbg
+
+            let s' = GHC.mkTyConApp tc args
+
+            let (exist, theta, xs) = GHC.dataConInstSig dc args
+            let fls                = GHC.dataConFieldLabels dc
+            unless (length xs == length fls) $ fail "|tys| /= |fls|"
+
+            idx <- case elemIndex fl fls of
+                Nothing  -> fail "field selector not in dataCon"
+                Just idx -> return idx
+
+            -- variables we can bind to
+            let exist' = exist
+            let exist_ = map GHC.mkTyVarTy exist'
+
+            theta' <- traverse (makeVar "dict") $ GHC.substTysWith exist exist_ theta
+            xs'   <- traverse (makeVar "x") $ GHC.substTysWith exist exist_ xs
+
+            let a' = xs !! idx
+            let b' = a'
+            let t' = s'
+
+            bName <- TC.unsafeTcPluginTcM $ TcM.newName (GHC.mkVarOcc "b")
+            let bBndr   = GHC.mkLocalId bName $ xs !! idx
+
+            -- (\b -> DC b x1 x2, x0)
+            let rhs = GHC.mkConApp (GHC.tupleDataCon GHC.Boxed 2)
+                    [ GHC.Type $ GHC.mkFunTy b' t'
+                    , GHC.Type a'
+                    , GHC.mkCoreLams [bBndr] $ GHC.mkConApp2 dc (args ++ exist_) $ theta' ++ replace idx bBndr xs'
+                    , GHC.Var $ xs' !! idx
+                    ]
+
+            -- (a -> r, r)
+            let caseType = GHC.mkTyConApp (GHC.tupleTyCon GHC.Boxed 2)
+                    [ GHC.mkFunTy b' t'
+                    , a'
+                    ]
+
+            -- DC x0 x1 x2 -> (\b -> DC b x1 x2, x0)
+            let caseBranch = (GHC.DataAlt dc, exist' ++ theta' ++ xs', rhs)
+
+            -- TC.tcPluginIO $ warn dflags l $
+            --     GHC.text "cases"
+            --     GHC.$$
+            --     GHC.ppr caseType
+            --     GHC.$$
+            --     GHC.ppr caseBranch
+
+
+            -- \s -> case s of DC x0 x1 x2 -> (\b -> DC b x1 x2, x0)
+            sName <- TC.unsafeTcPluginTcM $ TcM.newName (GHC.mkVarOcc "s")
+            let sBndr   = GHC.mkLocalId sName s'
+            let expr   = GHC.mkCoreLams [sBndr] $ GHC.Case (GHC.Var sBndr) sBndr caseType [caseBranch]
+            let evterm = makeEvidence4 hasPolyFieldCls expr tys
+
+            -- wanteds
+            ctEvidence <- TC.newWanted ctloc $ GHC.mkPrimEqPred a a'
+
+            return (evterm, [ TcM.mkNonCanonical ctEvidence -- a ~ a'
+                            ])
+
+    return $ TcRnTypes.TcPluginOk (mapMaybe extractA solved) (concat $ mapMaybe extractB solved)
+  where
+    wantedsHasPolyField = mapMaybe (findClassConstraint4 hasPolyFieldCls) wanteds
+
+    extractA (Nothing, _)     = Nothing
+    extractA (Just (a, _), b) = Just (a, b)
+
+    extractB (Nothing, _)      = Nothing
+    extractB (Just (_, ct), _) = Just ct
+
+replace :: Int -> a -> [a] -> [a]
+replace _ _ []     = []
+replace 0 y (_:xs) = y:xs
+replace n y (x:xs) = x : replace (pred n) y xs
+
+makeVar :: String -> GHC.Type -> TcRnTypes.TcPluginM GHC.Var
+makeVar n ty = do
+    name <- TC.unsafeTcPluginTcM $ TcM.newName (GHC.mkVarOcc n)
+    return (GHC.mkLocalId name ty)
+
+-------------------------------------------------------------------------------
+-- Simple Ct operations
+-------------------------------------------------------------------------------
+
+findClassConstraint4 :: Class.Class -> TcM.Ct -> Maybe (TcM.Ct, V4 GHC.Type)
+findClassConstraint4 cls ct = do
+   (cls', [k, x, s, a]) <- GHC.getClassPredTys_maybe (TcM.ctPred ct)
+   guard (cls' == cls)
+   return (ct, V4 k x s a)
+
+-- | Make newtype class evidence
+makeEvidence4 :: Class.Class -> GHC.CoreExpr -> V4 GHC.Type -> Tc.EvTerm
+makeEvidence4 cls e (V4 k x s a) = Tc.EvExpr appDc where
+    tyCon = Class.classTyCon cls
+    dc    = GHC.tyConSingleDataCon tyCon
+    appDc = GHC.mkCoreConApps dc
+        [ GHC.Type k
+        , GHC.Type x
+        , GHC.Type s
+        , GHC.Type a
+        , e
+        ]
+-------------------------------------------------------------------------------
+-- Adopted from GHC
+-------------------------------------------------------------------------------
+
+
+matchHasField
+    :: GHC.DynFlags
+    -> (FamInstEnv.FamInstEnv, FamInstEnv.FamInstEnv)
+    -> RdrName.GlobalRdrEnv
+    -> V4 GHC.Type
+    -> TcM.TcM (Maybe (GHC.TyCon, GHC.DataCon, [GHC.Type], GHC.FieldLabel, GHC.Id))
+matchHasField _dflags famInstEnvs rdrEnv (V4 _k x s _a)
+    -- x should be a literal string
+    | Just xStr <- GHC.isStrLitTy x
+    -- s should be an applied type constructor
+    , Just (tc, args) <- GHC.tcSplitTyConApp_maybe s
+    -- use representation tycon (if data family); it has the fields
+    , let s_tc = fstOf3 (FamInst.tcLookupDataFamInst famInstEnvs tc args)
+    -- x should be a field of r
+    , Just fl <- GHC.lookupTyConFieldLabel xStr s_tc
+    -- the field selector should be in scope
+    , Just _gre <- RdrName.lookupGRE_FieldLabel rdrEnv fl
+    -- and the type should have only single data constructor (for simplicity)
+    , Just [dc] <- GHC.tyConDataCons_maybe tc
+    = do
+        sel_id <- TcEnv.tcLookupId (GHC.flSelector fl)
+        (_tv_prs, _preds, sel_ty) <- TcMType.tcInstType TcMType.newMetaTyVars sel_id
+
+        -- The selector must not be "naughty" (i.e. the field
+        -- cannot have an existentially quantified type), and
+        -- it must not be higher-rank.
+        if not (GHC.isNaughtyRecordSelector sel_id) && GHC.isTauTy sel_ty
+        then return $ Just (tc, dc, args, fl, sel_id)
+        else return Nothing
+
+matchHasField _ _ _ _ = return Nothing
+
+-------------------------------------------------------------------------------
+-- Utils
+-------------------------------------------------------------------------------
+
+fstOf3 :: (a, b, c) -> a
+fstOf3 (a, _, _) =  a
+
